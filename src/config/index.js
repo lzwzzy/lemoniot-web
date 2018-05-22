03265850e0482183b131d03e668052a159b904ce
lzw <1129459766@qq.com>
2018-05-23 02:20:50 +0800

    v

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,10 @@
 export default {
   baseUrl: function () {
-    return process.env.NODE_ENV === 'development' ? 'http://lemond.free.ngrok.cc' : '//api.neho.top'
+    return process.env.NODE_ENV === 'development' ? '//lemond.free.ngrok.cc' : '//api.neho.top'
   },
   wx: {
     appid: 'wx863a2fdffc5b7050',
-    redirect_uri: 'https://lemoniot.cn/oauth',
+    redirect_uri: 'http://lemoniot.cn/oauth',
     scope: 'snsapi_userinfo'
   }
 }
